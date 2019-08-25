9b6a911de1b4c5f59631a01234ff3755504a5786
FarinHeiT <yariksuharik85@gmail.com>
2019-08-25 14:16:30 +0300

    Add flask-wtf for registration page

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
+# COnfigure app
 app = Flask(__name__)
 app.secret_key = 'topsecret'
 
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-	return render_template('index.html')
+
+	reg_form = RegistrationForm()
+
+	if reg_form.validate_on_submit():
+		print('great')
+
+	return render_template('index.html', form=reg_form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ wtform_fields.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+	""" Registration form """
+
+	username = StringField('username_label', validators=[InputRequired(message="Username required"),
+					Length(min=4, max=25, message="Username must be between 4 and 25 chars.")])
+	password = PasswordField('password_label', validators=[InputRequired(message="Password required"),
+					Length(min=6, max=25, message="Password must be between 4 and 25 chars.")])
+	confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password confirmation required"),
+					EqualTo('password', message="Password must match.")])
+
+	submit_button = SubmitField('Create')
